a31dd81f5fc084f325d0f358ae6dd3f82b0729dd
Giang Nam <giangnam10a2@gmail.com>
2021-08-10 21:30:10 +0700

    Connect socketIO

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from "vue";
 import axios from "axios";
 import VueAxios from "vue-axios";
-import VueRouter from "vue-router";
+import VueSocketIO from "vue-socket.io";
+import SocketIO from "socket.io-client";
 // plugins
 import vuetify from "./plugins/vuetify";
 // components
@@ -10,11 +11,20 @@ import App from "./App.vue";
 // other
 import store from "./store/store";
 import router from "./routes/router";
+import { BACKEND_SERVER } from "./configs/constants";
 // configs
-Vue.use(VueAxios, axios);
-Vue.use(VueRouter);
 Vue.config.productionTip = false;
 Vue.config.devtools = true;
+Vue.use(VueAxios, axios);
+
+Vue.use(
+  new VueSocketIO({
+    debug: true,
+    connection: SocketIO(BACKEND_SERVER, {
+      autoConnect: false,
+    }),
+  })
+);
 
 new Vue({
   vuetify,
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,5 +1,79 @@
 <template>
-  <div>
-    Login
-  </div>
+  <v-main class="main">
+    <v-form>
+      <v-container>
+        <v-row justify="center">
+          <v-col cols="12" sm="8" md="6">
+            <v-text-field
+              v-model="form.username"
+              label="Username"
+              placeholder="Username"
+              :rules="formRules.username"
+              required
+              outlined
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row justify="center">
+          <v-btn
+            color="primary"
+            elevation="2"
+            @click="login"
+            :disabled="isDisableButton"
+            large
+            >Go!</v-btn
+          >
+        </v-row>
+      </v-container>
+    </v-form>
+  </v-main>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      formRules: {
+        username: [(v) => !!v || "Username is required"],
+      },
+      form: {
+        username: "",
+      },
+    };
+  },
+  methods: {
+    login() {
+      const { username } = this.form;
+      this.$socket.auth = { username };
+      this.$socket.connect();
+    },
+  },
+  computed: {
+    isDisableButton() {
+      return this.form.username.length ? false : true;
+    },
+  },
+  sockets: {
+    connect() {
+      const user = {
+        username: this.$socket.auth.username,
+        socketId: this.$socket.id,
+      };
+      this.$store.dispatch("login", user);
+      this.$router.push("/");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.main {
+  background-image: url("https://www.wallpaperbetter.com/wallpaper/555/606/469/sea-sky-beach-2K-wallpaper.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+}
+.container {
+  margin-top: 200px;
+}
+</style>
